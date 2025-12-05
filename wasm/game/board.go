58c11ea3a0ccdd7f34a2dea865cb015fd85e4fbd
mdfarhan20 <mohamedfarhan201103@gmail.com>
2025-12-05 17:01:31 +0530

    added point and direction types for board

--- wasm/game/board.go
+++ wasm/game/board.go
@@ -12,7 +12,53 @@ const (
 // Board Dimensions
 const (
 	Height = 6
-	Width = 7
+	Width  = 7
 )
 
 type Board [Height][Width]Cell
+
+type Point struct {
+	X, Y int
+}
+
+func (p Point) Move(d Direction) Point {
+	offset := directionOffset[d]
+	return Point{p.X + offset.X, p.Y + offset.Y}
+}
+
+
+func (p Point) isValid() bool {
+	i, j := p.X, p.Y
+
+	if i < 0 || j < 0 || i >= Height || j >= Width {
+		return false
+	}
+
+	return true
+}
+
+type Direction uint8
+
+const (
+	Up Direction = iota
+	Down
+	Left
+	Right
+	RightUp
+	RightDown
+	LeftUp
+	LeftDown
+)
+
+var directions = []Direction{Up, Down, Left, Right, RightUp, RightDown, LeftUp, LeftDown}
+
+var directionOffset = [8]Point{
+	Up:        {-1, 0},
+	Down:      {1, 0},
+	Left:      {0, -1},
+	Right:     {0, 1},
+	RightUp:   {-1, 1},
+	RightDown: {1, 1},
+	LeftUp:    {-1, -1},
+	LeftDown:  {1, -1},
+}
